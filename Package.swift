63552523ee2a865d7469973b26bae65842e54afe
KarlHarnois <karl@goDynamo.com>
2018-09-13 00:06:13 -0400

    Remove quick from project
    Most of our test use XCTestCase instead of QuickSpec so refactor the
    remaining spec into a test case.

--- Package.swift
+++ Package.swift
@@ -5,11 +5,10 @@ import PackageDescription
 let package = Package(
   name: "compiler",
   dependencies: [
-    .package(url: "https://github.com/Quick/Quick.git", from: "1.3.1"),
     .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.1")
   ],
   targets: [
     .target(name: "Compiler", dependencies: []),
-    .testTarget(name: "CompilerTests", dependencies: ["Compiler", "Quick", "Nimble"])
+    .testTarget(name: "CompilerTests", dependencies: ["Compiler", "Nimble"])
   ]
  )
---
+++ Tests/CompilerTests/Helpers/StringExtensionTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+import Nimble
+@testable import Compiler
+
+class StringExtensionTests: XCTestCase {
+  func testSubscript() {
+    expect("123"[0]) == "3"
+  }
+
+  func testTrueMatch() {
+    expect("123".match("[0-9]")) == true
+  }
+
+  func testFalseMatch() {
+    expect("abc".match("[0-9]")) == false
+  }
+}
