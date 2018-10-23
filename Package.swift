6574c5cd7bc289ca62aea16be3fb970c1be43c0b
KarlHarnois <karl@goDynamo.com>
2018-10-23 13:07:17 -0400

    Update swift version and nimble package

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
   name: "compiler",
   dependencies: [
-    .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.1")
+    .package(url: "https://github.com/Quick/Nimble.git", from: "7.3.1")
   ],
   targets: [
     .target(name: "Compiler", dependencies: []),
